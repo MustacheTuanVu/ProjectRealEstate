20ca0945fd049b402b2551c723efcd93e42144f3
letancuong <tancuong.le.549@gmail.com>
2019-03-09 19:27:23 +0700

    Servlet page create estate

--- src/java/Servlet/Estate/EstateList.java
+++ src/java/Servlet/Estate/EstateList.java
@@ -28,7 +28,7 @@ public class EstateList extends HttpServlet {
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
-     *
+     * asadasdasdsadad
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
